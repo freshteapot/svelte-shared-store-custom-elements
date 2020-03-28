ebe2f8be69f8d4a497e4d1ad8920f1359ccf41cb
Christopher Williams <chris@freshteapot.net>
2020-03-28 11:18:27 +0100

    keeping the store simple to read

--- src/store.js
+++ src/store.js
@@ -2,24 +2,7 @@ import { writable } from 'svelte/store';
 
 const count = writable(0);
 
-const api = {
-    hello: () => {
-        console.log("yo")
-    }
-
-}
-const loggedIn = () => {
-    console.log("am I logged in")
-}
-
-const logout = () => {
-    console.log("I want to be logged out")
-}
-
 export {
-    count,
-    loggedIn,
-    logout,
-    api,
+    count
 }
 
